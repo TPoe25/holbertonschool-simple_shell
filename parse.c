ea495917873542eabf3a63b023649588d2485c2d
CourtneyAlv <voiceoftruth42@gmail.com>
2023-07-29 13:27:52 -0700

    added comments

--- parse.c
+++ parse.c
@@ -14,29 +14,29 @@ char **parse_string(char *line)
 	if (!tokens)
 	{
 		fprintf(stderr, "allocation error in parse.\n");
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); /*exit the program if mem allocate fails*/
 	}
 	token = strtok(line, TOKEN_DELIM);
 	while (token != NULL)
 {
-	if (token[0] == '#')
+	if (token[0] == '#') /* Handle Comments */
 	{
 		break;
 	}
-	token[s] = token;
+	token[s] = token; /*store the token in the tokens array*/
 	s++;
 	if (s >= bufsize)
-	{
+	{ /*reallocate memeory for tokens */
 		bufsize += bufsize;
 		tokens = realloc(tokens, bufsize * sizeof(char *));
 	if (!tokens)
 	{
 		fprintf(stderr, "reallocation error in parse.\n");
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); /* exit program if reallocation fails*/
 	}
 	}
 	token = strtok(NULL, TOKEN_DELIM);
 }
-	tokens[s] = NULL;
-	return (tokens);
+	tokens[s] = NULL; /*sets last element of the token arrayto NULL*/
+	return (tokens); /* Return the array of tokens */
 }
