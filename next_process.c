8bb91773362e361f6699070cc2b9108a741895d8
TPoe25 <t_poe25@yahoo.com>
2023-08-01 09:29:01 -0700

    trying to fix error

--- next_process.c
+++ next_process.c
@@ -1,44 +1,34 @@
 #include "basic_shell.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-
+/**
+ *next_process - creates a new process and executes the givven command
+ *@args: array of strings containing the command and its arguments
+ *Return: 1 on successful execution, 0 otherwise
+*/
 int next_process(char **args)
 {
-    pid_t pid;
-    int report;
+	pid_t pid;
+	int report;
 
-    struct stat st;
-    if (stat(args[0], &st) == 0 && S_ISREG(st.st_mode) && (st.st_mode & S_IXUSR))
-    {
-        pid = fork();
-        if (pid == 0)
-        {
-            char *env[] = {"PATH=", "", NULL};
-            if (execve(args[0], args, env) == -1)
-            {
-                perror("error in next_process");
-            }
-            exit(EXIT_FAILURE);
-        }
-        else if (pid < 0)
-        {
-            perror("forking: error in next_process");
-        }
-        else
-        {
-            do
-            {
-                waitpid(pid, &report, WUNTRACED);
-            } while (!WIFEXITED(report) && !WIFSIGNALED(report));
-        }
-    }
-    else
-    {
-        fprintf(stderr, "Error: %s is not a valid executable file.\n", args[0]);
-    }
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("error in next_process");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+		perror("forking: error in next_process");
+	}
+	else
+	{
+		do
 
-    return (-1);
+		{
+			waitpid(pid, &report, WUNTRACED);
+		}	while (!WIFEXITED(report) && !WIFSIGNALED(report));
+	}
+	return (-1);
 }
-
